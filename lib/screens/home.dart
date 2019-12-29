55cd23d9a9d95b40a4f5f31805aa4270116bfe82
athulantonynp <athulantonynp@gmail.com>
2019-12-29 18:03:48 +0530

    add kpi graph

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:bezier_chart/bezier_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:imadmin/models/user.dart';
 import 'package:imadmin/utils/admincolors.dart';
@@ -16,18 +17,29 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AdminColors.fromHex("#212121"),
-        body: Container(
+        body: SingleChildScrollView(
+
+          child: Container(
             color: AdminColors.fromHex("#212121"),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 getWelcomeText(widget.user.name),
-                Table(
-                  children: [_buildTableRow()],
-                )
+                Table(children: [_buildTableRow()],),
+                getKPITitle(),
+                getDummyChart(context)
               ],
-            )));
+            ),
+        )));
+  }
+
+  Padding getKPITitle(){
+    return Padding(
+      padding: EdgeInsets.fromLTRB(32, 32, 24, 0),
+      child: Text("KPI for your website",
+      style:TextStyle(fontSize: 26,color:Colors.white,fontWeight: FontWeight.bold)),
+    );
   }
 
   Padding getWelcomeText(String name) {
@@ -48,7 +60,7 @@ class HomeState extends State<Home> {
 
   TableRow _buildTableRow() {
     return TableRow(children: [
-      _getCardRandom("#FF9800", "Active Users", "30"),
+      _getCardRandom("#F44336", "Active Users", "30"),
       _getCardRandom("#607D8B", "Downloads", "12k"),
       _getCardRandom("#2196F3", "Rating", "4.5")
     ]);
@@ -86,5 +98,40 @@ Card _getCardRandom(String color, String title, String value) {
     ),
     elevation: 8,
     margin: EdgeInsets.all(16),
+  );
+}
+
+Center getDummyChart(BuildContext context){
+  return Center(
+    child: Container(
+      color: AdminColors.fromHex("#03A9F4"),
+      height: MediaQuery.of(context).size.height / 2,
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: BezierChart(
+        bezierChartScale: BezierChartScale.CUSTOM,
+        xAxisCustomValues: const [0, 5, 10, 15, 20, 25, 30, 35],
+        series: const [
+          BezierLine(
+            data: const [
+              DataPoint<double>(value: 10, xAxis: 0),
+              DataPoint<double>(value: 130, xAxis: 5),
+              DataPoint<double>(value: 50, xAxis: 10),
+              DataPoint<double>(value: 45, xAxis: 15),
+              DataPoint<double>(value: 75, xAxis: 20),
+              DataPoint<double>(value: 40, xAxis: 25),
+              DataPoint<double>(value: 105, xAxis: 30),
+              DataPoint<double>(value: 145, xAxis: 35),
+            ],
+          ),
+        ],
+        config: BezierChartConfig(
+          verticalIndicatorStrokeWidth: 6.0,
+          verticalIndicatorColor: Colors.black26,
+          showVerticalIndicator: true,
+          backgroundColor: AdminColors.fromHex("#212121"),
+          snap: false,
+        ),
+      ),
+    ),
   );
 }
