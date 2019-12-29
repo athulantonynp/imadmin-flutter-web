e32449269548870e17e55afc75df6c95c279f2e8
athulantonynp <athulantonynp@gmail.com>
2019-12-29 12:15:19 +0530

    add home screen content

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:imadmin/api/imadminapi.dart';
+import 'package:imadmin/models/user.dart';
 import 'package:imadmin/screens/home.dart';
 import 'package:imadmin/utils/admincolors.dart';
 import 'package:imadmin/widgets/appdrawer.dart';
 import 'package:imadmin/widgets/monsterbar.dart';
+
+import 'loginpage.dart';
 
 class DashBoard extends StatefulWidget {
   DashBoard({Key key}) : super(key: key);
@@ -13,13 +17,28 @@ class DashBoard extends StatefulWidget {
 
 
 class DashBoardState extends State<DashBoard>{
+
+  var _user=MonsterAdminApi().getUser();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MonsterBar(true),
       body: Container(
         color: AdminColors.fromHex("#212121"),
-        child: Home(),
+        child: FutureBuilder(
+        future: _user,
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          if(snapshot.hasData){
+            if(snapshot.data!=null){
+              return Home(snapshot.data);
+            }
+          }else{
+           return LoginPage(title: 'IM Admin');
+          }
+          
+        },
+      ),
       ),
       drawer:  AppDrawer().getDrawer(),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,21 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:imadmin/models/user.dart';
 import 'package:imadmin/utils/admincolors.dart';
 
 class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
+  final User user;
+
+  const Home(this.user);
 
   @override
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: AdminColors.fromHex("#212121"),
-      )
-    );
+        backgroundColor: AdminColors.fromHex("#212121"),
+        body: Container(
+            color: AdminColors.fromHex("#212121"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                getWelcomeText(widget.user.name),
+                Table(
+                  children: [_buildTableRow()],
+                )
+              ],
+            )));
   }
 
+  Padding getWelcomeText(String name) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(32, 24, 24, 16),
+        child: RichText(
+          text: TextSpan(
+            text: 'Welcome ',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+            children: <TextSpan>[
+              TextSpan(
+                  text: name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26))
+            ],
+          ),
+        ));
+  }
+
+  TableRow _buildTableRow() {
+    return TableRow(children: [
+      _getCardRandom("#FF9800", "Active Users", "30"),
+      _getCardRandom("#607D8B", "Downloads", "12k"),
+      _getCardRandom("#2196F3", "Rating", "4.5")
+    ]);
+  }
+}
+
+Card _getCardRandom(String color, String title, String value) {
+  return Card(
+    semanticContainer: true,
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    color: AdminColors.fromHex(color),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(24),
+          child: Text(
+            title,
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(24, 0, 16, 16),
+          child: Text(
+            value,
+            style: TextStyle(
+                color: Colors.white, fontSize: 80, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    ),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(16.0),
+    ),
+    elevation: 8,
+    margin: EdgeInsets.all(16),
+  );
 }
